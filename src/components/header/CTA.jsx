e6c21a04582e16f08796b0fb94c5b965cbe7985f
Nikhil Chitte <chnikhil250@gmail.com>
2022-07-07 14:48:13 +0530

    seo update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CV from '../../assets/cv.pdf';
+// import CV from '../../assets/cv.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">Download CV</a>
+      <a href='https://www.linkedin.com/in/nikhil-chitte-b97908219/' className="btn">Download CV</a>
       <a href="https://github.com/chittenikhil250" target="_blank" rel="noreferrer" className="btn btn-primary">View on GitHub </a>
     </div>
   )
